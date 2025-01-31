1fc968f583ad1d159941c5b82451fa0dddfed139
Benjamin Rothan <rothan.benjamin@gmail.com>
2025-01-31 21:07:03 +0100

    feat(archive): use of shutil instead

--- triage.py
+++ triage.py
@@ -9,6 +9,7 @@ from rich.console import Console
 from rich.table import Table
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import FuzzyWordCompleter
+import shutil
 
 console = Console()
 
@@ -147,7 +148,7 @@ class FileProcessor:
                 os.makedirs(year_folder, exist_ok=True)
                 old_path = os.path.join(self.inbox_path, filename)
                 new_path = os.path.join(year_folder, filename)
-                os.rename(old_path, new_path)
+                shutil.move(old_path, new_path)
                 console.print(f"[bold green]Archived file:[/bold green] [magenta][u]{filename}[/u][/magenta] to [magenta]{year_folder}[/magenta]")
 
 def main(
